8ce7d9bc354e011e5a5a8cae0f274b15af59c947
Lars <lars@elfskot.cloud>
2019-10-03 14:50:50 +0200

    opdracht2

--- Opdracht2/ParenthesisChecker.pas
+++ Opdracht2/ParenthesisChecker.pas
@@ -4,8 +4,9 @@ unit ParenthesisChecker;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Stack;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  Vcl.StdCtrls, Stack;
 
 type
   TParenthesisCheckerForm = class(TForm)
@@ -41,26 +42,31 @@ implementation
 
 procedure TParenthesisCheckerForm.ClearOutput;
 begin
+  { Uitvoer leegmaken. }
   MemoOutput.Lines.Clear;
 end;
 
 procedure TParenthesisCheckerForm.ButtonEmptyFormClick(Sender: TObject);
 begin
+  { Invoer en uitvoer leegmaken. }
   ClearForm;
 end;
 
 procedure TParenthesisCheckerForm.ButtonEmpyNotificationsClick(Sender: TObject);
 begin
+  { Uitvoer leegmaken. }
   ClearOutput;
 end;
 
 procedure TParenthesisCheckerForm.ButtonTryCheckClick(Sender: TObject);
 begin
   try
+    { Uitvoer leegmaken en haakjes controleren. }
     ClearOutput;
     CheckParenthesis;
   except
     on E: Exception do
+      { Alle foutmeldingen weergeven in het uitvoerveld. }
       AddLineToOutput(E.Message);
   end;
 end;
@@ -76,32 +82,41 @@ begin
   Stack := TStack.Create;
   Stack.Instantiate;
 
+  { Alle karakters in het invoerveld bij langsgaan. }
   for i := 1 to Length(EditInput.Text) do { String indexing is 1-based }
   begin
     CurrentChar := EditInput.Text[i];
 
+    { Als het karakter geen haake is, doorgaan naar het volgende karakter. }
     if not IsParenthesis(CurrentChar) then
       continue;
 
+    { Als het een sluithaakje is en er zit niets op de stack, dan
+      is dit een verkeerd sluithaakje. }
     if Stack.IsEmpty and IsClosingParenthesis(CurrentChar) then
     begin
       IsInputCorrect := False;
       AddLineToOutput('Sluithaakje teveel voor ' + CurrentChar + ' op positie '
         + IntToStr(i) + '.');
-      continue;
     end;
 
+    { Als het een openingshaakje is, dan deze toevoegen aan de stack. }
     if IsOpeningParenthesis(CurrentChar) then
       Stack.Push(CurrentChar);
 
-    if IsClosingParenthesis(CurrentChar) then
+    { Als het een sluithaakje is en er zitten element op de stack. }
+    if not Stack.IsEmpty and IsClosingParenthesis(CurrentChar) then
     begin
+      { Controleren of het sluithaakje overeenkomt met het element
+        op de stack. }
       if IsMatch(Stack.Peek, CurrentChar) then
       begin
+        { Haakje van de stack afhalen. }
         Stack.Pop;
       end
       else
       begin
+        { Haakje kwam niet overeen, dus een verkeerd sluithaakje. }
         IsInputCorrect := False;
         AddLineToOutput('Foutief sluithaakje ' + CurrentChar + ' op positie '
           + IntToStr(i) + '.');
@@ -109,18 +124,22 @@ begin
     end;
   end;
 
+  { Als er nog openingshaakjes op de stack zitten, dan hebben deze
+    geen sluithaakje. Hiervoor een foutmelding weergeven. }
   while not Stack.IsEmpty do
   begin
     IsInputCorrect := False;
     AddLineToOutput('Geen sluithaakje voor ' + Stack.Pop + '.');
   end;
 
+  { Als er geen fouten zijn, dan klopt de expressie. }
   if IsInputCorrect then
     AddLineToOutput('Alle haakjes zijn correct gepaard.');
 end;
 
 procedure TParenthesisCheckerForm.ClearForm;
 begin
+  { Formulier leegmaken en focus op het invoerveld leggen. }
   ClearOutput;
   EditInput.Clear;
   EditInput.SetFocus;
@@ -128,27 +147,43 @@ end;
 
 procedure TParenthesisCheckerForm.AddLineToOutput(Text: String);
 begin
+  { Regel toevoegen aan uitvoer. }
   MemoOutput.Lines.Add(Text);
+  { Een message queuen op de Windows Message Queue (Win32 API) om in het
+    tekstveld een regel naar beneden te scrollen.
+
+    SendMessage(Handle, Message, Parameter1, Parameter2)
+
+    Handle: is een verwijzing naar het formulier element.
+    WM_VSCROLL:  Window message om verticaal te scrollen.
+    SB_LINEDOWN: Parameter om aan te geven dat er 1 regel naarbeneden moet
+                 worden gescrolled.
+    0:           Lege parameter.
+  }
   SendMessage(MemoOutput.Handle, WM_VSCROLL, SB_LINEDOWN, 0); { Scroll to end. }
 end;
 
 function TParenthesisCheckerForm.IsParenthesis(Character: Char): Boolean;
 begin
+  { True als het een haakje is. }
   Result := IsOpeningParenthesis(Character) or IsClosingParenthesis(Character);
 end;
 
 function TParenthesisCheckerForm.IsOpeningParenthesis(Character: Char): Boolean;
 begin
+  { True als het een openingshaakje is. }
   Result := (Character = '[') or (Character = '(') or (Character = '{');
 end;
 
 function TParenthesisCheckerForm.IsClosingParenthesis(Character: Char): Boolean;
 begin
+  { True als het een sluithaakje is. }
   Result := (Character = ']') or (Character = ')') or (Character = '}');
 end;
 
 function TParenthesisCheckerForm.IsMatch(Left: Char; Right: Char): Boolean;
 begin
+  { True als de twee haakjes een match zijn, dus hetzelfde soort haakje. }
   Result := (Left = '[') and (Right = ']')
          or (Left = '(') and (Right = ')')
          or (Left = '{') and (Right = '}')
