3a8b838e4b642739e6335628a84b6ae59f0fb22f
Lars <lars@elfskot.cloud>
2019-12-11 14:00:09 +0100

    sudoku

---
+++ Sudoku/Sudoku.dpr
@@ -0,0 +1,14 @@
+program Sudoku;
+
+uses
+  Vcl.Forms,
+  Solver in 'Solver.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
